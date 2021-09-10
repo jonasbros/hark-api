e32f44a522f8a0da9cf61e6193c501a961b49289
Jonas Perez <00jonasperez@gmail.com>
2021-09-10 20:05:22 +0800

    user posts route

--- app/Http/Controllers/UserPostController.php
+++ app/Http/Controllers/UserPostController.php
@@ -7,19 +7,28 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 use App\Models\UserPost;
+use App\Models\User;
+
 
 class UserPostController extends Controller
 {
-    public function get(Request $request, $url) {
+    public function get(Request $request, $url, $perpage) {
+
+        $userId = User::where('custom_url', $url)->first()->id;
+
         if( Auth::check() ) {
-            $posts = UserPost::where('user_id', auth()->user()->id)->get();
+            $posts = UserPost::where('user_id', $userId)->limit($perpage)->get();
 
             return response()->json([
                 'status' => 'success', 
                 'posts' => $posts,
-            ]);
-        }else {
-            echo 'no atuhh!';
+            ], 200);
         }
+
+        return response()->json([
+            'status' => 'error', 
+            'message' => 'Unauthorized!',
+        ], 401);
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ $router->get('/', function () use ($router) {
 
 $router->group(['prefix' => 'api', 'middleware' => 'cors'], function () use ($router) {
     $router->get('/artists', 'ArtistController@index');
-    $router->get('/getposts/{url}', 'UserPostController@get');
+    $router->get('/getposts/{url}/{perpage}', 'UserPostController@get');
     
     
     $router->post('/me', 'UserController@me');
